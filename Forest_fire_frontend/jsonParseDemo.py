ed4ccdd44da8dc0352bb335118e07e84fcc21341
unknown <bhavyahingorani54@gmail.com>
2022-11-01 21:56:58 +0530

    Writer not working

--- Forest_fire_frontend/jsonParseDemo.py
+++ Forest_fire_frontend/jsonParseDemo.py
@@ -1,6 +1,27 @@
 import json
+import csv
+from csv import DictWriter
+import datetime
 
-callMeBack = '{\n  "Temperature": "38.671875",  "flameDetected": "1"\n}'
+field_names = ['Time', 'Temperature', 'Flame', 'Fire']
+
+def fireHere(temp, flame):
+    if float(temp) >= 40 and int(flame) == 1:
+        return True
+    return False
+
+callMeBack = '{"Temperature": "38.671875",  "flameDetected": "1"}'
+
 json_object = json.loads(callMeBack)
+fire = fireHere(json_object["Temperature"], json_object["flameDetected"])
+now = str(datetime.datetime.now())
 
-print(json_object["Temperature"])
+List = [now, json_object["Temperature"], json_object["flameDetected"], fire]
+Dict = {'Time': now, 'Temperature':json_object["Temperature"], 'Flame':json_object["flameDetected"], 'Fire':fire}
+
+with open('data.csv', 'a+') as f_object:    
+    dictwriter_object = DictWriter(f_object, fieldnames=field_names, lineterminator='\n')
+    dictwriter_object.writerow(Dict)
+    f_object.close()
+
+print("Done :)")
